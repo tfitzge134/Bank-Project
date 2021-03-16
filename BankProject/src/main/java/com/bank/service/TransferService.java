35019da3ac10d28dcb2b6f54019b5dbfe82ae417
TERESA Hidalgo De Fitzgerald <tfitzge134@gmail.com>
2021-03-16 08:00:39 -0500

    TransferService

--- BankProject/src/main/java/com/bank/service/TransferService.java
+++ BankProject/src/main/java/com/bank/service/TransferService.java
@@ -2,4 +2,5 @@ package com.bank.service;
 
 public interface TransferService {
 
+	public boolean Transfer(String sourceAccount, String destAccount, double transactionamount);
 }
---
+++ BankProject/src/main/java/com/bank/service/impl/TransferServiceImpl.java
@@ -0,0 +1,13 @@
+package com.bank.service.impl;
+
+import com.bank.service.TransferService;
+
+public class TransferServiceImpl implements TransferService {
+
+	@Override
+	public boolean Transfer(String sourceAccount, String destAccount, double transactionamount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
